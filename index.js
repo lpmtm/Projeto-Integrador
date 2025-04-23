1c6c179ca2f6c84670b9ca724f42676acd890f3b
Eduardo Lima <eduardoaluno1800@gmail.com>
2025-04-23 17:15:22 -0300

    adicionando arquivo principal do node

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get("/", (req,res)=>{
+    res.send("Bem vindo ao meu site");
+})
+
+app.listen(4000,function(erro){
+    if(erro){
+        console.log("Ocorreu um erro");
+    }else{
+        console.log("Iniciando servidor");
+    }
+})
