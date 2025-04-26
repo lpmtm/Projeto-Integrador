f9ac4921f5bf798c50df4ee4869515cb7ef27d1b
Eduardo Lima <eduardoaluno1800@gmail.com>
2025-04-26 18:56:59 -0300

    reorganizando arquivos de login

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express"); // iniciando express
 const path = require("path"); // adicionando path
 const bodyParser = require("body-parser"); // fazendo a conexão com a pasta public
 const connection = require("./database/database"); // conexão com banco de dados
-const Controller = require("./login/Controller"); // importando o controller
+
 const app = express();
 
 app.set('view engine', 'ejs'); 
@@ -20,9 +20,6 @@ connection.authenticate()
     .catch((error) => {
         console.log("Erro ao conectar ao banco de dados:", error);
     });
-
-
-app.use("/", Controller); 
 
 app.listen(8080, () => {
     console.log("Servidor rodando");
