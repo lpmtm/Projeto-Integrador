07985b3c339dfdc952d8a82d07d33e6f7168f8cd
carol <lpcarolinelopes@gmail.com>
2025-05-24 23:15:40 -0300

    ainda sem o banco de dados online, mas com umas mudanças

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const connection = new Sequelize("modelo", "root", "010203", {
+const connection = new Sequelize("modelo", "root", "adimin", {
   host: "localhost",
   dialect: "mysql",
 });
